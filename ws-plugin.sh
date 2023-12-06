35fb5648d1d264be1af21f2290ad97fd3a4afebe
ZeroCal Su <3146312184@qq.com>
2023-12-06 11:25:31 +0800

    Update ws-plugin.sh

--- ws-plugin.sh
+++ ws-plugin.sh
@@ -118,6 +118,7 @@ fi
 
 # 将用户输入的连接名称，链接地址和Token写入ws-plugin/config/config/ws-config.yaml
 cat << EOF >> ./plugins/ws-plugin/config/config/ws-config.yaml
+ 
   - name: $name
     address: $url
     type: 4
